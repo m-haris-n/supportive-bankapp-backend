06edf6e680d9bd168160e2fe366d57dbb820e3cd
M. Taha Ahmad <tahaahmad929@gmail.com>
2025-01-09 10:17:06 +0500

    Plaid route updated

---
+++ src/app/api/plaid/link_token/route.ts
@@ -0,0 +1,44 @@
+import { apiResponse } from "@/app/helpers/functions";
+import { NextRequest, NextResponse } from "next/server";
+import {
+  Configuration,
+  CountryCode,
+  PlaidApi,
+  PlaidEnvironments,
+  Products,
+} from "plaid";
+
+const configuration = new Configuration({
+  basePath: PlaidEnvironments.sandbox,
+  baseOptions: {
+    headers: {
+      "PLAID-CLIENT-ID": process.env.PLAID_CLIENT_ID,
+      "PLAID-SECRET": process.env.PLAID_SECRET,
+    },
+  },
+});
+
+const plaidClient = new PlaidApi(configuration);
+
+export const GET = async (request: any) => {
+  try {
+    const url = new URL(request.url);
+    const searchParams = new URLSearchParams(url.searchParams);
+    const id = searchParams.get("id") as string;
+
+    const response = await plaidClient.linkTokenCreate({
+      user: { client_user_id: id },
+      client_name: "Supportive App",
+      products: ["auth" as Products], // Use 'auth' for user authentication
+      country_codes: ["US" as CountryCode],
+      language: "en",
+    });
+
+    return apiResponse(true, response.data);
+  } catch (error) {
+    if (error instanceof Error) {
+      return apiResponse(false, { error: error.message }, 500);
+    }
+    return apiResponse(false, { error: "Unknown error" }, 500);
+  }
+};
